41e5ebabae65c155a27de44377a8de5e4e5d2dcd
지승렬 <seungryeol156@gmail.com>
2023-03-02 14:23:57 +0900

    Vue Router 학습: 게시판 UI 만들기

---
+++ vue3-posts/src/api/posts.js
@@ -0,0 +1,14 @@
+// axios로 데이터 조회
+const posts = [
+  {id:1, title:"제목1", content:"내용1", createdAt:"2020-01-01"},
+  {id:2, title:"제목2", content:"내용2", createdAt:"2021-01-01"},
+  {id:3, title:"제목3", content:"내용3", createdAt:"2022-01-01"},
+  {id:4, title:"제목4", content:"내용4", createdAt:"2023-01-01"},
+  {id:5, title:"제목5", content:"내용5", createdAt:"2024-01-01"},
+  {id:6, title:"제목6", content:"내용6", createdAt:"2025-01-01"},
+  {id:7, title:"제목7", content:"내용7", createdAt:"2026-01-01"},
+];
+
+export function getPosts(){
+  return posts;
+}
--- vue3-posts/src/layouts/TheHeader.vue
+++ vue3-posts/src/layouts/TheHeader.vue
@@ -30,24 +30,41 @@
                 About
               </RouterLink>
             </li>
+            <li class="nav-item">
+              <RouterLink
+                class="nav-link active"
+                to="/posts"
+                active-class="active"
+              >
+                게시글 목록
+              </RouterLink>
+            </li>
           </ul>
-          <form class="d-flex">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
+          <div class="d-flex">
+            <button
+              class="btn btn-outline-success text-white"
+              type="button"
+              @click="goPage"
+            >
+              글쓰기
             </button>
-          </form>
+          </div>
         </div>
       </div>
     </nav>
   </header>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goPage = () => {
+  router.push({
+    name: 'PostCreate',
+  });
+};
+</script>
 
 <style lang="scss" scoped></style>
