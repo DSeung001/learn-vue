37ab6f2b31b25f55db4998744febfd9da1bf3677
seungryeol <seungryeol156@gmail.com>
2023-03-04 10:05:26 +0900

    Alert 공통 컴포넌트

--- vue3-posts/src/views/posts/PostEditView.vue
+++ vue3-posts/src/views/posts/PostEditView.vue
@@ -5,23 +5,34 @@
     <PostForm
       v-model:title="form.title"
       v-model:content="form.content"
-      @submit.prevent="edit">
-
+      @submit.prevent="edit"
+    >
       <template #actions>
-          <button type="button" class="btn btn-outline-danger " @click="goDetailPage">취소</button>
-          <button class="btn btn-primary">수정</button>
+        <button
+          type="button"
+          class="btn btn-outline-danger"
+          @click="goDetailPage"
+        >
+          취소
+        </button>
+        <button class="btn btn-primary">수정</button>
       </template>
     </PostForm>
 
+    <AppAlert
+      :show="showAlert"
+      :message="alertMessage"
+      :type="alertType"
+    ></AppAlert>
   </div>
 </template>
 
 <script setup>
-
-import { useRoute, useRouter } from "vue-router";
-import { ref } from "vue";
-import { getPostById, updatePost } from "@/api/posts";
-import PostForm from "@/components/posts/PostForm.vue";
+import { useRoute, useRouter } from 'vue-router';
+import { ref } from 'vue';
+import { getPostById, updatePost } from '@/api/posts';
+import PostForm from '@/components/posts/PostForm.vue';
+import AppAlert from '@/components/AppAlert.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -29,11 +40,11 @@ const id = route.params.id;
 
 const form = ref({
   title: null,
-  content: null
+  content: null,
 });
 
 const fetchPost = async () => {
-  try{
+  try {
     const { data } = await getPostById(id);
     console.log(data);
     setForm(data);
@@ -50,15 +61,31 @@ const setForm = ({ title, content }) => {
 const edit = async () => {
   try {
     await updatePost(id, { ...form.value });
-    router.push({ name: "PostDetail", params: { id } });
+    vAlert('수정이 완료되었습니다.', 'success');
+    // router.push({name: "PostDetail", params: {id}});
   } catch (error) {
+    vAlert('네트워크 오류');
     console.log(error);
   }
 };
 
 fetchPost();
 
-const goDetailPage = () => router.push({ name: "PostDetail", params: { id } });
+const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
+
+const showAlert = ref(false);
+const alertMessage = ref('');
+const alertType = ref('');
+
+// Alert은 이미 쓰는 네이밍이니까
+const vAlert = (message, type = 'error') => {
+  showAlert.value = true;
+  alertMessage.value = message;
+  alertType.value = type;
+  setTimeout(() => {
+    showAlert.value = false;
+  }, 2000);
+};
 </script>
 
 <style scoped></style>
