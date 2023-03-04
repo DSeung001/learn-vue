005d1809534d0300bbbfbfb3909a4689343d9b52
seungryeol <seungryeol156@gmail.com>
2023-03-04 10:23:59 +0900

    Transition 컴포넌트

--- vue3-posts/src/components/AppAlert.vue
+++ vue3-posts/src/components/AppAlert.vue
@@ -1,7 +1,9 @@
 <template>
-  <div v-if="show" class="app-alert alert" :class="styleClass" role="alert">
-    {{ message }}
-  </div>
+  <Transition name="slide">
+    <div v-if="show" class="app-alert alert" :class="typeStyle" role="alert">
+      {{ message }}
+    </div>
+  </Transition>
 </template>
 
 <script setup>
@@ -22,8 +24,8 @@ const props = defineProps({
     validator: value => ['success', 'error'].includes(value),
   },
 });
-const styleClass = computed(() =>
-  props.type === 'error' ? 'alert-danger' : 'alert-success',
+const typeStyle = computed(() =>
+  props.type === 'error' ? 'alert-danger' : 'alert-primary',
 );
 </script>
 
@@ -32,5 +34,23 @@ const styleClass = computed(() =>
   position: fixed;
   top: 10px;
   right: 10px;
+}
+
+/* 트랜지션의 기본이 v-이고, 이름이 있으면 이름- 으로 대체*/
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.6s ease;
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  opacity: 1;
+  transform: translateY(0px);
 }
 </style>
